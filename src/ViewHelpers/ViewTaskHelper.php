766939a4c068e7cddd7836a5cca5834128d3982a
Belinda <belinda.a.williams@hotmail.co.uk>
2021-11-25 16:12:43 +0000

    styling changes

--- src/ViewHelpers/ViewTaskHelper.php
+++ src/ViewHelpers/ViewTaskHelper.php
@@ -14,10 +14,10 @@ class ViewTaskHelper
             $htmlStr .= '<td>' . $task->getTask() . '</td>';
             $htmlStr .= '<td>';
             $htmlStr .= '<td><form method="post" action="markComplete/' . $task->getId() . '">';
-            $htmlStr .= '<input type="submit" value ="Complete" class="btn btn-primary"></td>';
+            $htmlStr .= '<input type="submit" value ="Complete" class="btn custom-btn"></td>';
             $htmlStr .= '</form>';
             $htmlStr .= '<td><form method="post" action="deleteTask/' . $task->getId() . '">';
-            $htmlStr .= '<input type="submit" value ="Delete" class="btn btn-primary"></td>';
+            $htmlStr .= '<input type="submit" value ="Delete" class="btn custom-btn"></td>';
             $htmlStr .= '</form>';
             $htmlStr .= '</tr>';
         }
@@ -29,13 +29,13 @@ class ViewTaskHelper
         $htmlStr = '';
         foreach ($completedTasks as $task) {
             $htmlStr .= '<tr class="table-light">';
-            $htmlStr .= '<td>' . $task->getTask() . '</td>';
+            $htmlStr .= '<td><span class="completed-tasks">' . $task->getTask() . '</span></td>';
             $htmlStr .= '<td>';
             $htmlStr .= '<td><form method="post" action="undoComplete/' . $task->getId() . '">';
-            $htmlStr .= '<input type="submit" value ="Undo" class="btn btn-primary"></td>';
+            $htmlStr .= '<input type="submit" value ="Undo" class="btn custom-btn"></td>';
             $htmlStr .= '</form>';
             $htmlStr .= '<td><form method="post" action="deleteTask/' . $task->getId() . '">';
-            $htmlStr .= '<input type="submit" value ="Delete" class="btn btn-primary"></td>';
+            $htmlStr .= '<input type="submit" value ="Delete" class="btn custom-btn"></td>';
             $htmlStr .= '</form>';
             $htmlStr .= '</tr>';
         }
